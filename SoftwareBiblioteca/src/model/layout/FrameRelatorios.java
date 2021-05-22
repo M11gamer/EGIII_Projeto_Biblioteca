b05c3d48a608c4b03d3b2d4f298b8da6df303776
Mateus Iora <57507689+mateusiora@users.noreply.github.com>
2021-05-22 18:02:52 -0300

    Relatórios Prontos
    Relatórios de Alunos, Livros e Movimentação de livros adicionado!

--- SoftwareBiblioteca/src/model/layout/FrameRelatorios.java
+++ SoftwareBiblioteca/src/model/layout/FrameRelatorios.java
@@ -1,6 +1,12 @@
 
 package model.layout;
 
+import connection.ConnectionFactory;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.view.JasperViewer;
+
 
 public class FrameRelatorios extends javax.swing.JFrame {
 
@@ -36,17 +42,32 @@ public class FrameRelatorios extends javax.swing.JFrame {
         getContentPane().setLayout(null);
 
         jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/model/icon/icone_clientes.png"))); // NOI18N
-        jButton1.setText("Relatorios de Clientes");
+        jButton1.setText("Relatorios de Alunos");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
         getContentPane().add(jButton1);
         jButton1.setBounds(560, 180, 210, 59);
 
         jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/model/icon/icone_livros.png"))); // NOI18N
         jButton2.setText("Relatorio de Livros");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
         getContentPane().add(jButton2);
         jButton2.setBounds(80, 182, 210, 60);
 
         jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/model/icon/icone_retirada.png"))); // NOI18N
-        jButton3.setText("Livros Emprestados");
+        jButton3.setText("Movimentação");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
         getContentPane().add(jButton3);
         jButton3.setBounds(320, 180, 210, 60);
 
@@ -71,6 +92,63 @@ public class FrameRelatorios extends javax.swing.JFrame {
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
        dispose();
     }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+       java.sql.Connection conn = ConnectionFactory.getConnection();
+
+        String src = "ReportLivros.jasper";
+
+        JasperPrint jasperPrint = null;
+
+        try {
+            jasperPrint = JasperFillManager.fillReport(src, null, conn);
+        } catch (JRException ex) {
+            System.out.println("Error: "+ex);
+        }
+
+        JasperViewer view = new JasperViewer(jasperPrint,false);
+
+        view.setVisible(true);
+
+        
+        
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+       java.sql.Connection conn = ConnectionFactory.getConnection();
+
+        String src = "ReportAluno.jasper";
+
+        JasperPrint jasperPrint = null;
+
+        try {
+            jasperPrint = JasperFillManager.fillReport(src, null, conn);
+        } catch (JRException ex) {
+            System.out.println("Error: "+ex);
+        }
+
+        JasperViewer view = new JasperViewer(jasperPrint,false);
+
+        view.setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+       java.sql.Connection conn = ConnectionFactory.getConnection();
+
+        String src = "ReportMovimento.jasper";
+
+        JasperPrint jasperPrint = null;
+
+        try {
+            jasperPrint = JasperFillManager.fillReport(src, null, conn);
+        } catch (JRException ex) {
+            System.out.println("Error: "+ex);
+        }
+
+        JasperViewer view = new JasperViewer(jasperPrint,false);
+
+        view.setVisible(true);
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
